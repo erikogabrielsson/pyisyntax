b3b47e0b9cc4160a1e8839e818fe1c3e1b821c1f
Erik O Gabrielsson <erik.o.gabrielsson@sectra.com>
2024-10-28 23:38:22 +0100

    Test changing builder

--- isyntax_build/builder.py
+++ isyntax_build/builder.py
@@ -1,6 +1,7 @@
 import os
 from importlib import resources
 from pathlib import Path
+import platform
 
 from cffi import FFI
 
@@ -27,16 +28,24 @@ def create_ffibuilder() -> FFI:
 
     project_dir = Path(__file__).parent.parent
 
-    vendor = project_dir/"isyntax_build"/"vendor"
-    src = project_dir/"isyntax_build"/"src"
-    libisyntax_src = vendor/"libisyntax"/"src"
+    vendor = project_dir / "isyntax_build" / "vendor"
+    src = project_dir / "isyntax_build" / "src"
+    libisyntax_src = vendor / "libisyntax" / "src"
 
     if os.name == "nt":
         platform_sources = [
-            src/"win32_utils.c",
+            src / "win32_utils.c",
         ]
         extra_compile_args = ["/std:c11"]
         libraries = ["winmm"]
+    elif platform.processor() == "arm":
+        platform_sources = []
+        extra_compile_args = [
+            "-march=armv8.2-a+fp16+simd",
+            "-mfloat-abi=hard",
+            "-mfpu=neon-vfpv4",
+        ]
+        libraries = []
     else:
         platform_sources = []
         extra_compile_args = []
@@ -46,25 +55,25 @@ def create_ffibuilder() -> FFI:
         "isyntax._pyisyntax",
         resources.read_text(isyntax_build, "pyisyntax.c"),
         sources=paths_to_strings(
-            libisyntax_src/"libisyntax.c",
-            libisyntax_src/"isyntax"/"isyntax.c",
-            libisyntax_src/"isyntax"/"isyntax_reader.c",
-            libisyntax_src/"utils"/"timerutils.c",
-            libisyntax_src/"utils"/"block_allocator.c",
-            libisyntax_src/"utils"/"benaphore.c",
-            libisyntax_src/"platform"/"platform.c",
-            libisyntax_src/"platform"/"work_queue.c",
-            libisyntax_src/"third_party"/"yxml.c",
-            libisyntax_src/"third_party"/"ltalloc.cc",
-            src/"python_platform_utils.c",
+            libisyntax_src / "libisyntax.c",
+            libisyntax_src / "isyntax" / "isyntax.c",
+            libisyntax_src / "isyntax" / "isyntax_reader.c",
+            libisyntax_src / "utils" / "timerutils.c",
+            libisyntax_src / "utils" / "block_allocator.c",
+            libisyntax_src / "utils" / "benaphore.c",
+            libisyntax_src / "platform" / "platform.c",
+            libisyntax_src / "platform" / "work_queue.c",
+            libisyntax_src / "third_party" / "yxml.c",
+            libisyntax_src / "third_party" / "ltalloc.cc",
+            src / "python_platform_utils.c",
             *platform_sources,
         ),
         include_dirs=paths_to_strings(
             libisyntax_src,
-            libisyntax_src/"isyntax",
-            libisyntax_src/"platform",
-            libisyntax_src/"third_party",
-            libisyntax_src/"utils",
+            libisyntax_src / "isyntax",
+            libisyntax_src / "platform",
+            libisyntax_src / "third_party",
+            libisyntax_src / "utils",
             src,
         ),
         extra_compile_args=extra_compile_args,
@@ -72,13 +81,13 @@ def create_ffibuilder() -> FFI:
     )
 
     header_lines = []
-    with (libisyntax_src/"libisyntax.h").open() as f:
+    with (libisyntax_src / "libisyntax.h").open() as f:
         for line in f:
             if line.startswith("#"):
                 continue
             header_lines.append(line)
     header_lines.append("\n")
-    with (src/"python_platform_utils.h").open() as f:
+    with (src / "python_platform_utils.h").open() as f:
         for line in f:
             if line.startswith("#"):
                 continue
